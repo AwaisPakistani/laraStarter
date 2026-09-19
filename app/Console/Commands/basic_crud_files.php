a74ee966b3a35277d4e50dcc5806fceb7fcd4b07
Awais <awaisprojects1@gmail.com>
2026-09-19 13:54:29 +0500

    basic crud files added

---
+++ app/Console/Commands/basic_crud_files.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:basic_crud_files')]
+#[Description('Command description')]
+class basic_crud_files extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
